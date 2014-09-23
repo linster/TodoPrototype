4ba27353f65e480c99ec5a1329bbbf91b8d0b2f9
Stefan Martynkiw <linster247@gmail.com>
2014-09-23 15:07:14 -0600

    Deleting items works completely

--- src/com/example/smtd/multiselect/TMultiChoiceListener.java
+++ src/com/example/smtd/multiselect/TMultiChoiceListener.java
@@ -1,8 +1,12 @@
 package com.example.smtd.multiselect;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.example.smtd.R;
+import com.example.smtd.TItem;
 import com.example.smtd.TodoAdapter;
 
 
@@ -18,6 +22,11 @@ import android.widget.AbsListView.MultiChoiceModeListener;
 
 public class TMultiChoiceListener implements MultiChoiceModeListener {
 
+	
+	//Private hashmap of which positions were selected
+	private HashMap<Integer, Boolean> selectedpositions = new HashMap<Integer, Boolean>();
+	
+	
 	private TodoAdapter adapter;
 	
 	
@@ -32,7 +41,24 @@ public class TMultiChoiceListener implements MultiChoiceModeListener {
 		// TODO Auto-generated method stub
 		switch(item.getItemId()){	
 			case R.id.action_delete:
-				//deleteSelectedItems()
+				
+				// Build an array list of TItems to delete
+				ArrayList<TItem> itemstodelete = new ArrayList<TItem>();
+				
+				/* Delete the selected items */
+				for (Map.Entry<Integer, Boolean> entry : selectedpositions.entrySet()){
+					//Entry is in selectedpositions and value is true (to be deleted)
+					//note that items can be in this map, but set to false if they
+					//have been selected and deselected before the CAB is exited.
+					if (entry.getValue()) {
+						itemstodelete.add(adapter.getItem(entry.getKey()));
+					}
+				}
+				
+				for (TItem todoitem : itemstodelete) {
+					adapter.remove(todoitem);
+				}
+				adapter.notifyDataSetChanged();
 				mode.finish();
 			case R.id.action_email:
 				//emailSelectedItems()
@@ -54,15 +80,14 @@ public class TMultiChoiceListener implements MultiChoiceModeListener {
 
 	@Override
 	public void onDestroyActionMode(ActionMode mode) {
-		// Cleanup our selected flag.
+		// Cleanup our selected flags.
 
 		int position;
-		
 		for (position = 0; position < adapter.getCount(); position++){
 			adapter.getItem(position).setSelected(false);
 			adapter.notifyDataSetChanged();
 		}	
-		
+		selectedpositions.clear();
 		
 	}
 
@@ -77,6 +102,7 @@ public class TMultiChoiceListener implements MultiChoiceModeListener {
 			boolean checked) {
 		
 		//in here, update tmSelectedItems instance with data on what is selected
+		selectedpositions.put(position, checked);
 		adapter.getItem(position).setSelected(checked);
 		adapter.notifyDataSetChanged();
 		

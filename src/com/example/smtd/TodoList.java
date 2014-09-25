7616f6462da8c8992ea24a4fea48c96f12a31d78
Stefan Martynkiw <linster247@gmail.com>
2014-09-25 15:17:20 -0600

    Added docs/. Added liscences

--- src/com/example/smtd/TodoList.java
+++ src/com/example/smtd/TodoList.java
@@ -1,3 +1,17 @@
+/*Copyright 2014 Stefan Martynkiw
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.example.smtd;
 
 import java.io.*;
@@ -5,8 +19,6 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 import com.example.smtd.datahandler.FileBasedDataStore;
-import com.example.smtd.multiselect.TContextBar;
-
 import com.example.smtd.multiselect.TMultiChoiceListener;
 
 
--- src/com/example/smtd/datahandler/FileBasedDataStore.java
+++ src/com/example/smtd/datahandler/FileBasedDataStore.java
@@ -1,3 +1,17 @@
+/*Copyright 2014 Stefan Martynkiw
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.example.smtd.datahandler;
 
 import java.io.BufferedInputStream;
@@ -75,6 +89,11 @@ public class FileBasedDataStore extends ItemDataStore {
 		return null;
 	}
 
+	/* Implementing ISerializable. No code taken from site.*/
+	/* http://www.onjava.com/pub/a/onjava/excerpt/JavaRMI_10/?page=3 */
+	
+	
+	
 	@Override
 	public boolean PackData(ArrayList<TItem> source, String filename) {
 		/** Pack the in-memory ArrayList<TItem>. This serializes
@@ -86,6 +105,14 @@ public class FileBasedDataStore extends ItemDataStore {
 		
 		/* Need to serialize an object to a string 
 		 */
+		
+		/*
+		 * Creative Commons Attribution
+		 * http://creativecommons.org/licenses/by-sa/2.5/
+		 * 
+		 * Changes made to variable names.
+		 */
+		
 		// http://stackoverflow.com/questions/8887197/reliably-convert-any-object-to-string-and-then-back-again
 		// Creative Commons Attribution Share Alike license for the code
 		Log.d("FBDS pack data", source.toString());
